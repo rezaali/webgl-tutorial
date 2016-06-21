eb8d3fc964d4099eaadc4894f48ba250b73adc72
rezaali <syed.reza.ali@gmail.com>
2016-06-21 16:32:09 -0700

    removed unneeded things

--- index.js
+++ index.js
@@ -13,15 +13,11 @@ var glslify = require('glslify');
 // Import Math Libraries
 var mat4 = require('gl-matrix').mat4;
 
-//Import Web Helper Libraries
+// Import Web Helper Libraries
 var fit = require('canvas-fit');
 var isMobile = require('is-mobile');
 
-//Import YCAM GRP Libraries
-var ycam = require('ycam');
-var cam = require('nsc')( canvas );
-
-//Set the canvas size to fill the window and its pixel density
+// Set the canvas size to fill the window and its pixel density
 var mobile = isMobile( navigator.userAgent );
 var dpr = mobile ? 1 : ( window.devicePixelRatio || 1 );
 window.addEventListener( 'resize', fit( canvas, null, dpr ), false );
@@ -36,48 +32,23 @@ var vertexShader = glslify( './shaders/shader.vert' );
 var fragmentShader = glslify( './shaders/shader.frag' );
 var shader = glShader( gl, vertexShader, fragmentShader );
 
-//Setup Geometry for rendering
-var geo = glGeometry( gl );
-geo.attr( 'aPosition', ycam.positions, { size: 2 } );
-geo.faces( ycam.cells );
-var color = [ 1.0, 1.0, 1.0, 1.0 ];
-
-//Setup Sketch Variables
-var height;
-var width;
-
 function update() {
-  //set projection
-  width  = gl.drawingBufferWidth;
-  height = gl.drawingBufferHeight;
+  // Set Perspective Projection
   var aspectRatio = gl.drawingBufferWidth / gl.drawingBufferHeight;
   var fieldOfView = Math.PI / 4.0;
   var near = 0.01;
   var far  = 1000.0;
   mat4.perspective( projection, fieldOfView, aspectRatio, near, far );
-  //get view from camera
-  cam.view( view );
-  cam.update();
 }
 
 function render() {
   update();
 
-  gl.viewport( 0, 0, width, height );
+  gl.viewport( 0, 0, gl.drawingBufferWidth, gl.drawingBufferHeight );
   clear( gl );
 
-  //set blending
+  // Set Blending
   gl.disable( gl.DEPTH_TEST );
   gl.enable( gl.BLEND );
   gl.blendFunc( gl.SRC_ALPHA, gl.ONE );
-
-  geo.bind( shader );
-  if( isMobile ) { shader.uniforms.dpr = dpr * 2.0; } else { shader.uniforms.dpr = dpr; }
-  shader.uniforms.uPointSize = 1.0;
-  shader.uniforms.uProjection = projection;
-  shader.uniforms.uView = view;
-  shader.uniforms.uModel = model;
-  shader.uniforms.uColor = color;
-  geo.draw( gl.TRIANGLES );
-  geo.unbind();
 }
