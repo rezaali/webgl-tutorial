db355c1b7a3cd9b5bf8388d5f4c27ebb0aa3167e
rezaali <syed.reza.ali@gmail.com>
2016-06-22 01:29:12 -0700

    created 3D mesh, visualized, extruded and exportable

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ var glslify = require('glslify');
 
 // Import Math Libraries
 var mat4 = require('gl-matrix').mat4;
+var mat3 = require('gl-matrix').mat3;
 
 // Import Web Helper Libraries
 var fit = require('canvas-fit');
@@ -22,6 +23,8 @@ var keyPressed = require('key-pressed');
 var ycam = require('ycam');
 var cga = require('cga');
 var lgp = require('lgp');
+var mda = require('mda');
+var guf = require('guf');
 var cam = require('nsc')( canvas, { position: [ 0.0, 0.0, -75.0 ] } );
 
 // Set the canvas size to fill the window and its pixel density
@@ -32,12 +35,18 @@ window.addEventListener( 'resize', fit( canvas, null, dpr ), false );
 //Setup Matricies
 var projection = mat4.create();
 var model = mat4.create();
+var normalm4 = mat4.create();
+var normalm3 = mat3.create();
 var view = mat4.create();
 
 //Setup Shaders
 var vertexShader = glslify( './shaders/shader.vert' );
 var fragmentShader = glslify( './shaders/shader.frag' );
 var shader = glShader( gl, vertexShader, fragmentShader );
+
+var vertexMeshShader = glslify( './shaders/solid.vert' );
+var fragmentMeshShader = glslify( './shaders/solid.frag' );
+var meshShader = glShader( gl, vertexMeshShader, fragmentMeshShader );
 
 // Setup Sketch Parameters
 var len = ycam.positions.length;
@@ -61,6 +70,21 @@ var triangles = glGeometry( gl );
 triangles.attr( 'aPosition', ycam.positions, { size: 2 } );
 triangles.faces( cga.triangulatePolygon2( ycam.positions ) );
 
+// Mesh Variables
+var mesh, meshOutput, meshOutputTriangles;
+var positions, cells;
+var meshGeo;
+
+mesh = mda.ProfileGenerator( ycam.positions );
+mda.ExtrudeOperator( mesh, 0, 10, 0.0 );
+mda.ExtrudeOperator( mesh, 0, 0, 0.25 );
+mda.ExtrudeOperator( mesh, 1, 0, 4.25 );
+mda.PipeOperator( mesh, 0, 1, 1 );
+mda.TriangulateOperator( mesh );
+positions = mesh.getPositions();
+cells = mesh.getCells();
+meshGeo = createGeo( positions, cells );
+
 function update() {
   // Set Perspective Projection
   var aspectRatio = gl.drawingBufferWidth / gl.drawingBufferHeight;
@@ -70,6 +94,11 @@ function update() {
   mat4.perspective( projection, fieldOfView, aspectRatio, near, far );
   cam.view( view );
   cam.update();
+
+  mat4.copy( normalm4, view );
+  mat4.invert( normalm4, normalm4 );
+  mat4.transpose( normalm4, normalm4 );
+  mat3.fromMat4( normalm3, normalm4 );
 }
 
 function render() {
@@ -87,6 +116,7 @@ function render() {
   drawGeo( outlineExpanded, gl.LINE_LOOP, [ 0, 1, 1, 1 ] );
   drawGeo( hull, gl.LINE_LOOP, [ 1, 0, 0, 1 ] );
   drawGeo( triangles, gl.LINE_LOOP, [ 1, 1, 0, 1 ] );
+  drawMesh( meshGeo, [ 1, 1, 1, 1 ] );
 }
 
 window.addEventListener( 'keydown', function( event ) {
@@ -99,6 +129,12 @@ window.addEventListener( 'keydown', function( event ) {
       strokeWidth: 0.1,
     } ] ) );
   }
+  else if( keyPressed( 'E' ) ) {
+    lgp.fileWriter( "ycam_tutorial.stl", lgp.stlSerializer( { positions: positions, cells: cells } ) );
+    lgp.fileWriter( "ycam_tutorial.obj", lgp.objSerializer( { positions: positions, cells: cells } ) );
+    return;
+}
+
 }, false );
 
 function drawGeo( geo, mode, color ) {
@@ -111,4 +147,40 @@ function drawGeo( geo, mode, color ) {
   shader.uniforms.uColor = color;
   geo.draw( mode );
   geo.unbind();
+}
+
+function drawMesh( geo, color ) {
+  if( geo ) {
+    gl.enable( gl.DEPTH_TEST );
+    gl.enable( gl.BLEND );
+    gl.blendFunc( gl.SRC_ALPHA, gl.ONE_MINUS_SRC_ALPHA );
+    geo.bind( meshShader );
+    if( isMobile ) { meshShader.uniforms.dpr = dpr * 2.0; } else { meshShader.uniforms.dpr = dpr; }
+    meshShader.uniforms.uPointSize = 1.0;
+    meshShader.uniforms.uProjection = projection;
+    meshShader.uniforms.uView = view;
+    meshShader.uniforms.uNormalMatrix = normalm3;
+    meshShader.uniforms.uModel = model;
+    meshShader.uniforms.uColor = color;
+    geo.draw( gl.TRIANGLES );
+    geo.unbind();
+  }
+}
+
+function createGeo( positions, cells ) {
+  var newPositions = [];
+  var newNormals = [];
+
+  for( var i = 0; i < cells.length; i++ ) {
+    var a = positions[ cells[ i ][ 0 ] ];
+    var b = positions[ cells[ i ][ 1 ] ];
+    var c = positions[ cells[ i ][ 2 ] ];
+    var n = guf.calculateNormal( a, b, c );
+    newPositions.push( a, b, c );
+    newNormals.push( n, n, n );
+  }
+  var geo = glGeometry( gl );
+  geo.attr( 'aPosition', newPositions );
+  geo.attr( 'aNormal', newNormals );
+  return geo;
 }
